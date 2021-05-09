52d5427fee701782e9adcd3183b7134d2abf7307
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2021-05-09 13:04:38 -0300

    Add Tank

---
+++ Tank.java
@@ -0,0 +1,53 @@
+
+public class Tank extends Personagem{
+	
+	private boolean espTank = false;
+    
+    public Tank(String nome) {
+        super(nome);
+        this.setVida(150);
+        this.setRange(3);
+    }
+    @Override
+    public void atacar(Personagem pAtacado){
+        System.out.println(this.getNome()+"Socando o inimigo: "+pAtacado.getNome());
+        pAtacado.setVida(pAtacado.getVida() - 3);
+
+    }
+    @Override
+    public void ataqEsp(Personagem pAtacado){
+    	System.out.println(this.getNome()+"impedindo o inimigo "+pAtacado.getNome()+"de atacar durante 1 rodada");
+    	setEsp(true);
+    }
+    @Override
+    public void movimentar(){
+        
+    }
+	public boolean getEsp() {
+		return espTank;
+	}
+	public void setEsp(boolean espTank) {
+		this.espTank = espTank;
+	}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
