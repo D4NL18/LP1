071e6bd9e13d56bef8154bedf92d7e3859650e82
racoba <brunoracobaldo@hotmail.com>
2021-05-10 16:43:18 -0300

    sem saco p fzer tudo

--- Magos.java
+++ Magos.java
@@ -14,10 +14,14 @@ public class Magos extends PersonagemComMana{
         this.setVida(150);
         this.setRange(3);
         this.setMana(2);
+        if(this.getNome()=="Dr. Estranho")this.setVisual("M1");
+        else if(this.getNome()=="Toge Inumaki")this.setVisual("M2");
+        
     }
     @Override
     public void atacar(Personagem pAtacado){
-        System.out.println(this.getNome()+" Mandando a flecha no inimigo: "+pAtacado.getNome());
+        if(this.getNome()=="Dr. Estranho")System.out.println(this.getNome()+" atacou  inimigo: "+pAtacado.getNome()+"com sua magia");
+        else if(this.getNome()=="Toge Inumaki")System.out.println(this.getNome()+" atacou  inimigo: "+pAtacado.getNome()+"com sua magia");
         pAtacado.setVida(pAtacado.getVida() - 3);
         this.setMana(getMana()+1);
     }
@@ -25,15 +29,13 @@ public class Magos extends PersonagemComMana{
     public void ataqEsp(Personagem pAtacado){
         manaM = getMana();
         if(manaM == 3){
-            System.out.println(this.getNome()+" Mandando a flecha no inimigo: "+pAtacado.getNome());
+            if(this.getNome()=="Dr. Estranho")System.out.println(this.getNome()+" usou sua jóia do tempo para paralisar a atacar o inimigo: "+pAtacado.getNome());
+            else if(this.getNome()=="Toge Inumaki")System.out.println(this.getNome()+" recitou sua palavra proibida e inflingou grandes danos em: "+pAtacado.getNome());
             pAtacado.setVida(pAtacado.getVida() - 6);
             this.setMana(0);
         }
     }
-    @Override
-    public void movimentar(){
-        
-    }
+    
     
     
     
--- Personagem.java
+++ Personagem.java
@@ -2,18 +2,23 @@ package racobafoda.lp1;
 
 
 public abstract class Personagem {
+
+    
 	private String nome;
+        public String visual;
 	private int vida;
 	private int defesa;
 	private int item;
 	private int range;
-	private int posicao[][];
+	
 	
 	public abstract void atacar(Personagem pAtacado);
+        
+        public abstract void ataqEsp(Personagem pAtacado);
 	
-	public abstract void movimentar();
 	
-	public abstract void ataqEsp(Personagem pAtacado);
+	
+	
 	
 	
 	public String getNome() {
@@ -46,13 +51,16 @@ public abstract class Personagem {
 	public void setRange(int range) {
 		this.range = range;
 	}
-	public int[][] getPosicao() {
-		return posicao;
-	}
-	public void setPosicao(int posicao[][]) {
-		this.posicao = posicao;
-	}
+	
         
+    
+        public String getVisual() {
+            return visual;
+        }
+        public void setVisual(String visual) {
+            this.visual = visual;
+        }
+
         
         public Personagem(String nome){
         this.nome = nome;
