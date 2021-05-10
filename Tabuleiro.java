bc3c488bec2de671894657501f28f9490b362a39
racoba <brunoracobaldo@hotmail.com>
2021-05-10 16:45:37 -0300

    sem saco p escfrever bonitin

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,38 +1,98 @@
+package racobafoda.lp1;
+
 
 public class Tabuleiro {
-	private Personagem peca;
-	public char visual;
-	
-	public void inserir(Personagem personagem, int x, int y, Tabuleiro[][] tabuleiro) {
-		tabuleiro[x][y].setPeca(personagem);
-	}
 
-	public void preencher(int tam, Tabuleiro[][] tabuleiro) {
-			for(int i = 0; i<tam; i++) {
-				for(int j = 0; j<tam; j++) {
-					tabuleiro[i][j].visual = '.';
-				}
-			}
+    
+        public Tabuleiro(int op){
+            this.op = op;
+        }
+        private int op;
+        private Personagem[][] tabu = new Personagem[10][10];
+        private Personagem[][] tabu2 = new Personagem[12][12];
+        private Personagem[][] tabu3 = new Personagem[15][15];
+       
+	public void inserir(Personagem personagem, int x, int y) {
+            if(op==1){
+                if(tabu[x][y]==null){
+                tabu[x][y] = personagem;
+                tabu[x][y].visual = personagem.visual;
+                }else System.out.println("Deu erro no inserir");
+            }else if(op==2){
+                if(tabu2[x][y]==null){
+                tabu2[x][y] = personagem;
+                tabu2[x][y].visual = personagem.visual;
+                }else System.out.println("Deu erro no inserir");
+            }else if(op==3){
+                if(tabu3[x][y]==null){
+                tabu3[x][y] = personagem;
+                tabu3[x][y].visual = personagem.visual;
+                }else System.out.println("Deu erro no inserir");
+            }
+            
 		
 	}
+        
+        public void remover(int x, int y){
+            if(op==1){
+                if(tabu[x][y]!=null){
+                tabu[x][y]=null;
+                }else System.out.println("Deu erro no remover");
+            }else if(op==2){
+                if(tabu2[x][y]!=null){
+                tabu2[x][y]=null;
+                }else System.out.println("Deu erro no remover");
+            }else if(op==3){
+                if(tabu3[x][y]!=null){
+                tabu3[x][y]=null;
+                }else System.out.println("Deu erro no remover");
+            }
+            
+        }
 	
-	public void imprimir(int tam, Tabuleiro[][] tabuleiro) {
-		for(int i = 0; i<tam; i++) {
-			for(int j = 0; j<tam; j++) {
-				System.out.println(tabuleiro[i][j].visual);
+	public void imprimir() {
+            if(op==1){
+                for(int i = 0; i<tabu.length; i++) {
+			for(int j = 0; j<tabu.length; j++) {
+				if(tabu[i][j]==null){//by dev pedrao
+                                    System.out.print('.');
+                                }else{
+                                    System.out.print(tabu[i][j].getVisual());
+                                }
 			}
+                        System.out.println();
 		}
-		
+            }else if(op==2){
+                for(int i = 0; i<tabu2.length; i++) {
+			for(int j = 0; j<tabu2.length; j++) {
+				if(tabu2[i][j]==null){//by dev pedrao
+                                    System.out.print('.');
+                                }else{
+                                    System.out.print(tabu2[i][j].getVisual());
+                                }
+			}
+                        System.out.println();
+		}
+            }else if(op==3){
+                for(int i = 0; i<tabu3.length; i++) {
+			for(int j = 0; j<tabu3.length; j++) {
+				if(tabu3[i][j]==null){//by dev pedrao
+                                    System.out.print('.');
+                                }else{
+                                    System.out.print(tabu3[i][j].getVisual());
+                                }
+			}
+                        System.out.println();
+		}
+            }
 	}
-	
 
-	public Personagem getPeca() {
-		return peca;
-	}
 
-	public void setPeca(Personagem peca) {
-		this.peca = peca;
-	}
-
+       
+        
+        
+    
+        
+         
 	
 }
--- main.java
+++ main.java
@@ -2,11 +2,20 @@ package racobafoda.lp1;
 public class main {
 
 	public static void main(String[] args) {
-		int tam = 10;
-		Tabuleiro[][] tabuleiro = new Tabuleiro[tam][tam];
+		int op = 1;
+		Tabuleiro tabu = new Tabuleiro(op);
+                Magos m1 = new Magos("Dr. Estranho");
+                Magos m2 = new Magos("Toge Inumaki");
+            
+                
+                tabu.inserir(m1,1,1);
+                tabu.inserir(m2,1,3);
+                tabu.imprimir();
+                tabu.remover(1,1);
+                tabu.imprimir();
+                tabu.remover(1,3);
+                tabu.imprimir();
 		
-		tabuleiro.preencher(10, tabuleiro);
-		tabuleiro.imprimir(10, tabuleiro);
 	}
 
 }
