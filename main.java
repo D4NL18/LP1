574fdc5bd7a24bd5ccb820aeaa2a7be55a601a06
racoba <brunoracobaldo@hotmail.com>
2021-05-10 19:35:49 -0300

    [FEAT][MAIN] - Added movimentar() function on class Tabuleiro

--- main.java
+++ main.java
@@ -1,21 +1,25 @@
 package racobafoda.lp1;
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		int op = 1;
-		Tabuleiro tabu = new Tabuleiro(op);
                 Magos m1 = new Magos("Dr. Estranho");
                 Magos m2 = new Magos("Toge Inumaki");
+                Scanner ler = new Scanner(System.in);
+                System.out.println("Querido usuário, digite a opção de campo que você deseja:");
+                System.out.println("1=10x10");
+                System.out.println("2=12x12");
+                System.out.println("3=15x15");
+		int op = ler.nextInt();
+		Tabuleiro tabu = new Tabuleiro(op);
+                
             
                 
                 tabu.inserir(m1,1,1);
                 tabu.inserir(m2,1,3);
                 tabu.imprimir();
-                tabu.remover(1,1);
-                tabu.imprimir();
-                tabu.remover(1,3);
-                tabu.imprimir();
-		
+                tabu.movimentar(1, 1);
+		tabu.imprimir();
 	}
 
 }
