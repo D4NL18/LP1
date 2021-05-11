4a0f396062a253e0d504e7832ed0de09093a55d6
racoba <brunoracobaldo@hotmail.com>
2021-05-10 21:01:31 -0300

    [FEAT][MAIN] - Finished movimentar() in all boards

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,5 +1,5 @@
 package racobafoda.lp1;
-
+import java.util.Scanner;
 
 public class Tabuleiro {
 
@@ -7,6 +7,7 @@ public class Tabuleiro {
         public Tabuleiro(int op){
             this.op = op;
         }
+            
         private int op;
         private Personagem[][] tabu = new Personagem[10][10];
         private Personagem[][] tabu2 = new Personagem[12][12];
@@ -30,7 +31,6 @@ public class Tabuleiro {
                 }else System.out.println("Deu erro no inserir");
             }
             
-		
 	}
         
         public void remover(int x, int y){
@@ -54,7 +54,7 @@ public class Tabuleiro {
             if(op==1){
                 for(int i = 0; i<tabu.length; i++) {
 			for(int j = 0; j<tabu.length; j++) {
-				if(tabu[i][j]==null){//by dev pedrao
+				if(tabu[i][j]==null){
                                     System.out.print('.');
                                 }else{
                                     System.out.print(tabu[i][j].getVisual());
@@ -65,7 +65,7 @@ public class Tabuleiro {
             }else if(op==2){
                 for(int i = 0; i<tabu2.length; i++) {
 			for(int j = 0; j<tabu2.length; j++) {
-				if(tabu2[i][j]==null){//by dev pedrao
+				if(tabu2[i][j]==null){
                                     System.out.print('.');
                                 }else{
                                     System.out.print(tabu2[i][j].getVisual());
@@ -76,7 +76,7 @@ public class Tabuleiro {
             }else if(op==3){
                 for(int i = 0; i<tabu3.length; i++) {
 			for(int j = 0; j<tabu3.length; j++) {
-				if(tabu3[i][j]==null){//by dev pedrao
+				if(tabu3[i][j]==null){
                                     System.out.print('.');
                                 }else{
                                     System.out.print(tabu3[i][j].getVisual());
@@ -85,9 +85,72 @@ public class Tabuleiro {
                         System.out.println();
 		}
             }
+            
 	}
 
+        public void movimentar(int x, int y){
+            Scanner ler = new Scanner(System.in);
+            int nx,ny;
+            if(op==1){           
+                if(tabu[x][y]!=null) {
+                    System.out.println("Digite para onde você deseja mover a peça: ");
+                    nx = ler.nextInt();
+                    ny = ler.nextInt();
+                    if((nx==x+1 || nx==x-1 || nx==x) && (ny==y-1 || ny==y+1 || ny==y)){
+                        if(tabu[nx][ny]==null){
+                        inserir(tabu[x][y],nx,ny);
+                        remover(x,y);
+                        }else{
+                        System.out.println("Não é possível inserir uma peça nesse local, insira uma coordenada válida");
+                        movimentar(x,y);
+                        }
+                    }else{
+                        System.out.println("Não é possível inserir uma peça nesse local, insira uma coordenada válida");
+                        movimentar(x,y);
+                    }
+                }else System.out.println("As coordenadas digitadas não são válidas, digite outra peça para mover");
+                
+            }else if(op==2){      
+                if(tabu2[x][y]!=null) {
+                    System.out.println("Digite para onde você deseja mover a peça: ");
+                    nx = ler.nextInt();
+                    ny = ler.nextInt();
+                    if((nx==x+1 || nx==x-1 || nx==x) && (ny==y-1 || ny==y+1 || ny==y)){
+                        if(tabu2[nx][ny]==null){
+                        inserir(tabu2[x][y],nx,ny);
+                        remover(x,y);
+                        }else{
+                        System.out.println("Não é possível inserir uma peça nesse local, insira uma coordenada válida");
+                        movimentar(x,y);
+                        }
+                    }else{
+                        System.out.println("Não é possível inserir uma peça nesse local, insira uma coordenada válida");
+                        movimentar(x,y);
+                    }
+                }else System.out.println("As coordenadas digitadas não são válidas, digite outra peça para mover");  
+                
+            }else if(op==3){             
+                if(tabu3[x][y]!=null) {
+                    System.out.println("Digite para onde você deseja mover a peça: ");
+                    nx = ler.nextInt();
+                    ny = ler.nextInt();
+                    if((nx==x+1 || nx==x-1 || nx==x) && (ny==y-1 || ny==y+1 || ny==y)){
+                        if(tabu3[nx][ny]==null){
+                        inserir(tabu3[x][y],nx,ny);
+                        remover(x,y);
+                        }else{
+                        System.out.println("Não é possível inserir uma peça nesse local, insira uma coordenada válida");
+                        movimentar(x,y);
+                        }
+                    }else{
+                        System.out.println("Não é possível inserir uma peça nesse local, insira uma coordenada válida");
+                        movimentar(x,y);
+                    }
 
+                }else System.out.println("As coordenadas digitadas não são válidas, digite outra peça para mover");
+            }
+            
+        }
        
         
         
