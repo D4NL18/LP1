546c253f9caabcc6023e66c166df7cf8c4b7580d
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2021-06-01 16:33:21 -0300

    fx

---
+++ ControllerMenu.java
@@ -0,0 +1,12 @@
+package racobafoda.lp1;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class ControllerMenu {
+    
+    @FXML
+    private void IrParaRegras() throws IOException {
+        App.setRoot("Regras");
+    }
+}
